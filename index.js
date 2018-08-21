f6eb0d8aa1f56730eb02efbf1fd6168bb06c6d57
gillianbc <gszemeti@gmail.com>
2018-08-21 10:11:07 +0100

    Same but callback func in named variable

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ var rect = require('./rectangle');
 
 function solveRectangle(length,width){
     console.log("Solving for rec with length " + length + " and width " + width);
-
-    rect(length,width,(err,rectangle) => {
+    var myfunc = (err,rectangle) => {
         if (err){
             console.log("ERROR " + err.message);
         }
         else {
             console.log("Perim for length " + length + " width " + width + " is " + rectangle.perimeter());
-        }
-    });
+        };
+    };
+    rect(length,width,myfunc);
 
     console.log("After the call to rect");
-
 };
 
 solveRectangle(2,4);
