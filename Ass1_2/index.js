d9e4fe4e38affb20d70fff2b18715c75879374d7
GROUP\Gillian.Bladen-Clark <gillian.bladen-clark@olmgroup.com>
2018-08-25 20:11:49 +0100

    Ass1_2 finished - all assignment done

--- Ass1_2/index.js
+++ Ass1_2/index.js
@@ -1,42 +1,18 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 
-const dishRouter = require('./routes/dishRouter');
-
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use('/dishes', dishRouter);
 
-
-//============
-
-
-//when any post requests come in for /dishes/:dishId
-app.post('/dishes/:dishId', (req, res, next) => {
-    res.statusCode = 403; 
-    res.end('POST not supported on /dishes/:' + req.params.dishId);
-});
-
-//when any put requests come in for /dishes/:dishId
-app.put('/dishes/:dishId', (req, res, next) => {
-    res.write('Updating dish ' + req.params.dishId 
-        + ' with name ' + req.body.name + ' details ' + req.body.description);
-    //TO DO - code for updating dish
-    res.end('I have updated ' + req.params.dishId);
-});
-
-//when any delete requests come in for /dishes/:dishId
-app.delete('/dishes/:dishId', (req, res, next) => {
-    //TO DO - code to delete dish
-    res.end('Deleted ' + req.params.dishId);
-});
+app.use('/promos', promoRouter);
+app.use('/leaders', leaderRouter);
 
 //Tell express where to get static files from
 app.use(express.static(__dirname + '/public'));
