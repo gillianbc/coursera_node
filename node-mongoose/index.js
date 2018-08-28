181a249077b1bf40804da8ecc62f182944dbd19e
gillianbc <gbc@tesco.net>
2018-08-28 21:24:18 +0100

    exercise mongoose-odm-part-1

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -1,36 +1,38 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/conFusion';
-const dbname = 'conFusion';
+const Dishes = require('./models/dishes');
 
-MongoClient.connect(url, (err, client) => {
+const url = 'mongodb://localhost:27018/conFusion';
+const connect = mongoose.connect(url);
 
-    assert.equal(err,null);
+connect.then((db) => {
 
     console.log('Connected correctly to server');
 
-    const db = client.db(dbname);
-    const collection = db.collection("dishes");
-    collection.insertOne({"name": "Uthappizza", "description": "test"},
-    (err, result) => {
-        assert.equal(err,null);
-
-        console.log("After Insert:\n");
-        console.log(result.ops);
-
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err,null);
-            
-            console.log("Found:\n");
-            console.log(docs);
-
-            db.dropCollection("dishes", (err, result) => {
-                assert.equal(err,null);
-
-                client.close();
-            });
-        });
+    var newDish = Dishes({
+        name: 'Uthappizza',
+        description: 'test'
     });
 
+    newDish.save()
+        .then((dish) => {
+            console.log(dish);
+
+            return Dishes.find({});
+        })
+        .then((dishes) => {
+            console.log(dishes);
+
+            return Dishes.remove({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+})
+.catch((err) => {
+    console.log('Problem connecting to mongo db');
 });
