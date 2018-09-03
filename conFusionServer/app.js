0749c92f648018069613c09876f944f35c11f4aa
gillianbc <gbc@tesco.net>
2018-09-03 22:05:50 +0100

    Refactored the auth into its own module

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -9,6 +9,8 @@ const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 const Promotions = require('./models/promotions');
 const Leaders = require('./models/leaders');
+// authentication
+const auth = require('./authentication');
 //local modules for routes
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -44,43 +46,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser('12345-67890-09876-54321')); //secret key
-//Authentication
-function auth (req, res, next) {
-
-  if (!req.signedCookies.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-        var err = new Error('Computer says no - authentication requied!');
-        res.setHeader('WWW-Authenticate', 'Basic');              
-        err.status = 401;
-        next(err);
-        return;
-    }
-    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == 'admin' && pass == 'password') {
-        res.cookie('user','admin',{signed: true});
-        next(); // authorized
-    } 
-    else {
-        var err = new Error('I said no! You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');              
-        err.status = 401;
-        next(err);
-    }
-  }
-  else {
-      if (req.signedCookies.user === 'admin') {
-          next();
-      }
-      else {
-          var err = new Error('You are not authenticated!');
-          err.status = 401;
-          next(err);
-      }
-  }
-}
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
