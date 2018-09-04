18511fd7c2270d8a27ac0b55a101f9680b635c78
gillianbc <gbc@tesco.net>
2018-09-04 20:24:58 +0100

    Refactored the auth module more to my liking

--- conFusionServer/authentication.js
+++ conFusionServer/authentication.js
@@ -1,41 +1,46 @@
-const cookieParser = require('cookie-parser');
-
 //Authentication
 function auth (req, res, next) {
-
-    if (!req.signedCookies.user) {
-      var authHeader = req.headers.authorization;
-      if (!authHeader) {
-          var err = new Error('Computer says no - authentication requied!');
-          res.setHeader('WWW-Authenticate', 'Basic');              
-          err.status = 401;
-          next(err);
-          return;
-      }
-      var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-      var user = auth[0];
-      var pass = auth[1];
-      if (user == 'admin' && pass == 'password') {
-          res.cookie('user','admin',{signed: true});
-          next(); // authorized
-      } 
-      else {
-          var err = new Error('I said no! You are not authenticated!');
-          res.setHeader('WWW-Authenticate', 'Basic');              
-          err.status = 401;
-          next(err);
-      }
+    const authHeader = req.headers.authorization;
+    //No cookie and no auth details in header - bye bye
+    if (!req.signedCookies.user && !authHeader) {
+        var err = new Error('Computer says no - tell me who you are!');
+        res.setHeader('WWW-Authenticate', 'Basic');              
+        err.status = 401;
+        next(err);
+        // return;  //do we need this?
     }
-    else {
+    
+    //Has a cookie, but is it the right one?
+    if (req.signedCookies.user) {
+        console.log("User found in cookie: " + req.signedCookies.user);
         if (req.signedCookies.user === 'admin') {
             next();
         }
         else {
-            var err = new Error('You are not authenticated!');
+            var err = new Error('I checked your cookie but you are not the one!');
             err.status = 401;
             next(err);
         }
     }
+    
+    if (authHeader){
+        //Get the user and password from the header and set the cookie
+        var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+        var user = auth[0];
+        var pass = auth[1];
+        if (user == 'admin' && pass == 'password') {
+            res.cookie('user','admin',{signed: true});
+            console.log("Set the cookie on the response: " + res);
+            next(); // authorized
+        } 
+        else {
+            var err = new Error('I said no! You are not authenticated!');
+            res.setHeader('WWW-Authenticate', 'Basic');              
+            err.status = 401;
+            next(err);
+        }
+    }
+    console.log("End of authentication");
   };
 
   module.exports = auth;
