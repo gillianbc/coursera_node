46b01e7fe417d810b3eaeff1293a30c6a7385247
gillianbc <gszemeti@gmail.com>
2018-09-23 00:08:13 +0100

    Ass 4 - think everything is working now

--- conFusionServer/routes/favoriteRouter.js
+++ conFusionServer/routes/favoriteRouter.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const Favorites = require('../models/favorites');
 const Dishes = require('../models/dishes');
 const authenticate = require('../authenticate');
-const cors = require('./cors'); 
+const cors = require('./cors');
 const favoriteRouter = express.Router();
 favoriteRouter.use(bodyParser.json());
 
@@ -16,166 +16,155 @@ favoriteRouter.use(bodyParser.json());
 
 //Just for my benefit - not part of the assignment
 favoriteRouter.route('/all')
-.options(cors.corsWithOptions, authenticate.verifyUser, (req, res) => { res.sendStatus(200); })
-.get(cors.cors, (req,res,next) => {
-    Favorites.find({})
-    .populate('user')
-    .then((faves) => {
-        successResponse(faves,200,res);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-});
+    .options(cors.corsWithOptions, authenticate.verifyUser, (req, res) => { res.sendStatus(200); })
+    .get(cors.cors, (req, res, next) => {
+        Favorites.find({})
+            .populate('user')
+            .then((faves) => {
+                successResponse(faves, 200, res);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
 
 favoriteRouter.route('/')
-.options(cors.corsWithOptions, authenticate.verifyUser, (req, res) => { res.sendStatus(200); })
-.get(cors.cors,authenticate.verifyUser, (req,res,next) => {
-    //db.favorites.findOne({user: ObjectId("5b9ebf8de9557c3374e8a255")})
-    Favorites.findOne({'user' : req.user._id})
-    .populate('user')
-    .populate('dishes')
-    .then((resp) => {
-        if (resp) 
-            successResponse(resp,200,res);
-        else
-            notFound('Favourites list not found for ' + req.user.username);
+    .options(cors.corsWithOptions, authenticate.verifyUser, (req, res) => { res.sendStatus(200); })
+    .get(cors.cors, authenticate.verifyUser, (req, res, next) => {
+        //db.favorites.findOne({user: ObjectId("5b9ebf8de9557c3374e8a255")})
+        Favorites.findOne({ 'user': req.user._id })
+            .populate('user')
+            .populate('dishes')
+            .then((resp) => {
+                if (resp)
+                    successResponse(resp, 200, res);
+                else
+                    notFound('Favourites list not found for ' + req.user.username);
+            })
+            .catch((err) => next(err));
     })
-    .catch((err) => next(err));
-})
-.post(cors.corsWithOptions,authenticate.verifyUser, (req, res, next) => {
-    console.log('post user is ' + req.user._id);
-    addBodyDishesToFaveList(req,res,next)
-})
-.put(cors.corsWithOptions, (req, res, next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /');
-})
-.delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
-    Favorites.remove({"user" : req.user._id})
-    .then((resp) => {
-        if (resp) 
-            successResponse(resp,200,res);
-        else
-            notFound('Favourites list not found for ' + req.user.username);
+    .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        console.log('post user is ' + req.user._id);
+        addDishToFaveList(req, res, next, null, req.body);
     })
-    .catch((err) => next(err));    
-});
+    .put(cors.corsWithOptions, (req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /');
+    })
+    .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        Favorites.remove({ "user": req.user._id })
+            .then((resp) => {
+                if (resp)
+                    successResponse(resp, 200, res);
+                else
+                    notFound('Favourites list not found for ' + req.user.username);
+            })
+            .catch((err) => next(err));
+    });
 
 favoriteRouter.route('/:dishId')
-.get(cors.cors, authenticate.verifyUser, (req,res,next) => {
-    res.statusCode = 403;
-    res.end('GET operation not supported on //'+ req.params.dishId);
-})
-.post(cors.corsWithOptions, authenticate.verifyUser,  (req, res, next) => {
-    addDishToFaveList(req, res, next,req.params.dishId,true);
-})
-.put((req, res, next) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on //'+ req.params.dishId);
-})
-.delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
-    var dish;
-    Dishes.findById(req.params.dishId)
-    .then((fetchedDish) => {
-        if (fetchedDish == null) 
-            notFound("Dish not found");
-        //hang on to the dish, we'll need it later
-        //return the result of fetching the user's favourites list
-        dish = fetchedDish;  
-        return Favorites.findOne({"user" : req.user._id});
+    .get(cors.cors, authenticate.verifyUser, (req, res, next) => {
+        res.statusCode = 403;
+        res.end('GET operation not supported on //' + req.params.dishId);
     })
-    .then((favelist) => {
-        if (!favelist)
-            notFound("Favourite list not found for " + req.user.username);
-        if (favelist && isDishInList(favelist,dish)) {
-            favelist.dishes.remove(dish._id);
-            return favelist.save();
-        }
+    .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        addDishToFaveList(req, res, next, req.params.dishId, null);
     })
-    .then((resp) => {
-        successResponse(resp,200,res);
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('POST operation not supported on //' + req.params.dishId);
     })
-    .catch((err) => next(err));
-});
+    .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        console.log('In delete by dishId');
+        Favorites.findOne({ "user": req.user._id })
+        .then((favelist) => {
+            if (!favelist)
+                notFound("Favourite list not found for " + req.user.username);
+            if (isDishInList(favelist, req.params.dishId)) {
+                console.log('removing dish');
+                favelist.dishes.remove(req.params.dishId);
+                return favelist.save();
+            }
+            else
+                notFound("Dish not found in list");
+        })
+        .then((resp) => {
+            successResponse(resp, 200, res);
+        })
+        .catch((err) => next(err));
+    });
 
-function isDishInList(favourites, dish){
-    let position = -1;
+
+function pushDish(favelist, dishId) {
+    if (favelist.dishes && favelist.dishes.indexOf(dishId) < 0){
+        console.log('Pushing dish');
+        favelist.dishes.push({ _id: dishId });
+    }
+    else {
+        console.log('No work for me to do');
+    }
+    return favelist;
+}
+// This isn't right but surely I shouldn't need this anyhow
+// Schema validation should prevent an invalid dish being saved
+async function isValidDish(dishId) {
+    var dish = await Dishes.findById(dishId);
+    if (dish){
+        console.log('Dish is valid');
+        return true;
+    }
+    else {
+        console.log('Dish is not valid');
+        return false;
+    }
+        
+}
+function isDishInList(favourites, dishId) {
     if (favourites.dishes && favourites.dishes.length > 0) {
-        position = favourites.dishes.indexOf(dish._id);
-        if (position < 0)
-            return false;
-        else
-            return true; 
+        return favourites.dishes.indexOf(dishId) >= 0;
     }
 };
 
-function successResponse(responseItem, httpStatus,res){
-        res.statusCode = httpStatus;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(responseItem);
+function successResponse(responseItem, httpStatus, res) {
+    res.statusCode = httpStatus;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(responseItem);
 };
-function notFound(errorText){
+function notFound(errorText) {
     err = new Error(errorText);
     err.status = 404;
     throw err;
     // return next(err);
 }
-function addBodyDishesToFaveList(req, res, next){
-    var dishId;
-    var favelist;
-    console.log('addBodyDishesToFaveList user is ' + req.user._id);
-    for (var i in req.body) { 
-        if (req.body[i].hasOwnProperty('_id')) { 
-            dishId = req.body[i]._id;
-            console.log('The item id is ' + req.body[i]._id); 
-            addDishToFaveList(req, res, next, dishId, false)
-            .then
-        } 
-    }
-    Favorites.findOne({"user" : req.user._id})
-    .then((favelist) => {
-        successResponse(favelist,200,res),(err) => next(err)
-    })
-    .catch((err) => next(err));
-    
-}
-function addDishToFaveList(req, res, next, dishId, dishes){
+function addDishToFaveList(req, res, next, dishId, dishIds) {
     console.log('Dish id is ' + dishId);
-    var dish;
-    //Get the dish from Mongo
-    Dishes.findById(dishId)
-    .then((fetchedDish) => {
-        if (!fetchedDish) 
-            notFound(req,next,'Dish ' + dishId + ' not found');
-        dish = fetchedDish;  //hang on to the dish, we'll need it later
-        //return the result of fetching the user's favourites list
-        return Favorites.findOne({"user" : req.user._id});
-    })
-    .then((favelist) => {
-        if (!favelist){
-            //return the result of creating a favelist
-            console.log('Creating favelist');
-            return Favorites.create({user : req.user._id})
-        }
-        else
-            return favelist;
-    })
-    .then((favelist) => {
-        if (!isDishInList(favelist,dish)) {
-            favelist.dishes.push({_id : dish._id});
-            console.log('Adding dish ' + dish._id);
-            //return the result of saving the favelist
+    Favorites.findOne({ "user": req.user._id })
+        .then((favelist) => {
+            if (!favelist) {
+                //return the result of creating a favelist
+                console.log('Creating favelist');
+                return Favorites.create({ user: req.user._id })
+            }
+            else
+                return favelist;
+        })
+        .then((favelist) => {
+            if (dishId) {
+                pushDish(favelist, dishId);
+            }
+            if (dishIds) {
+                for (var i in dishIds) {
+                    if (dishIds[i].hasOwnProperty('_id')) {
+                        pushDish(favelist, dishIds[i]._id);
+                    }
+                }
+            }
             return favelist.save();
-        }
-        else {
-            //no need to error, just no need to add the dish
-            console.log('Dish was already there');
-            //return the favelist as is
-            return favelist;
-        }
-    })
-    .then((favelist) => {
-        successResponse(favelist,200,res),(err) => next(err);
-    })
-    .catch((err) => next(err));
+        })
+        .then((favelist) => {
+            successResponse(favelist, 200, res), (err) => next(err);
+        })
+        .catch((err) => next(err));
+
+
+
 }
 module.exports = favoriteRouter;
