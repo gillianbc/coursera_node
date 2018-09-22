f50a179c30ed9c7edcb33bd2286358cf3be1d579
gillianbc <gszemeti@gmail.com>
2018-09-22 18:20:01 +0100

    Ass 4 - error handling tidied

--- conFusionServer/routes/favoriteRouter.js
+++ conFusionServer/routes/favoriteRouter.js
@@ -21,7 +21,7 @@ favoriteRouter.route('/all')
     Favorites.find({})
     .populate('user')
     .then((faves) => {
-        success(faves,200,res);
+        successResponse(faves,200,res);
     }, (err) => next(err))
     .catch((err) => next(err));
 });
@@ -30,17 +30,19 @@ favoriteRouter.route('/')
 .options(cors.corsWithOptions, authenticate.verifyUser, (req, res) => { res.sendStatus(200); })
 .get(cors.cors,authenticate.verifyUser, (req,res,next) => {
     //db.favorites.findOne({user: ObjectId("5b9ebf8de9557c3374e8a255")})
-    console.log("The user is " + req.user);
     Favorites.findOne({'user' : req.user._id})
     .populate('user')
     .populate('dishes')
-    .then((faves) => {
-        success(faves,200,res);
-    }, (err) => next(err))
+    .then((resp) => {
+        if (resp) 
+            successResponse(resp,200,res);
+        else
+            notFound('Favourites list not found for ' + req.user.username);
+    })
     .catch((err) => next(err));
 })
 .post(cors.corsWithOptions,authenticate.verifyUser, (req, res, next) => {
-    addDishToFaveList(req,res,next)
+    addBodyDishesToFaveList(req,res,next)
 })
 .put(cors.corsWithOptions, (req, res, next) => {
     res.statusCode = 403;
@@ -49,8 +51,11 @@ favoriteRouter.route('/')
 .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
     Favorites.remove({"user" : req.user._id})
     .then((resp) => {
-        success(resp,200,res);
-    }, (err) => next(err))
+        if (resp) 
+            successResponse(resp,200,res);
+        else
+            notFound('Favourites list not found for ' + req.user.username);
+    })
     .catch((err) => next(err));    
 });
 
@@ -68,24 +73,25 @@ favoriteRouter.route('/:dishId')
 })
 .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
     var dish;
-    //Get the dish from Mongo
     Dishes.findById(req.params.dishId)
     .then((fetchedDish) => {
-        if (fetchedDish == null) noDish(req,next);
-        dish = fetchedDish;  //hang on to the dish, we'll need it later
+        if (fetchedDish == null) 
+            notFound("Dish not found");
+        //hang on to the dish, we'll need it later
         //return the result of fetching the user's favourites list
+        dish = fetchedDish;  
         return Favorites.findOne({"user" : req.user._id});
     })
     .then((favelist) => {
-        if (favelist != null && isDishInList(favelist,dish)) {
+        if (!favelist)
+            notFound("Favourite list not found for " + req.user.username);
+        if (favelist && isDishInList(favelist,dish)) {
             favelist.dishes.remove(dish._id);
             return favelist.save();
         }
-        else
-            (err) => next(err)
     })
     .then((resp) => {
-        success(resp,200,res);
+        successResponse(resp,200,res);
     })
     .catch((err) => next(err));
 });
@@ -93,10 +99,7 @@ favoriteRouter.route('/:dishId')
 function isDishInList(favourites, dish){
     let position = -1;
     if (favourites.dishes && favourites.dishes.length > 0) {
-        console.log('The favelist dishes are:' + favourites.dishes + ' looking for ' + dish._id);
-        console.log('Index ' + favourites.dishes.indexOf(dish._id));
         position = favourites.dishes.indexOf(dish._id);
-        console.log('Dish in fave list is ' + position);
         if (position < 0)
             return false;
         else
@@ -104,28 +107,30 @@ function isDishInList(favourites, dish){
     }
 };
 
-function success(responseItem, httpStatus,res){
+function successResponse(responseItem, httpStatus,res){
         res.statusCode = httpStatus;
         res.setHeader('Content-Type', 'application/json');
         res.json(responseItem);
 };
-function noDish(req,next){
-    err = new Error('Dish ' + req.params.dishId + ' not found');
+function notFound(errorText){
+    err = new Error(errorText);
     err.status = 404;
-    return next(err);
+    throw err;
+    // return next(err);
 }
 function addDishToFaveList(req, res, next){
     var dish;
     //Get the dish from Mongo
     Dishes.findById(req.params.dishId)
     .then((fetchedDish) => {
-        if (fetchedDish == null) noDish(req,next);
+        if (!fetchedDish) 
+            notFound(req,next,'Dish ' + req.params.dishId + ' not found');
         dish = fetchedDish;  //hang on to the dish, we'll need it later
         //return the result of fetching the user's favourites list
         return Favorites.findOne({"user" : req.user._id});
     })
     .then((favelist) => {
-        if (favelist == null)
+        if (!favelist)
             //return the result of creating a favelist
             return Favorites.create({user : req.user._id})
         else
@@ -133,8 +138,8 @@ function addDishToFaveList(req, res, next){
             return favelist;
     })
     .then((favelist) => {
+        if (!favelist) notFound("No fave list (unexpected)");
         if (!isDishInList(favelist,dish)) {
-            console.log('Adding dish');
             favelist.dishes.push({_id : dish._id});
             //return the result of saving the favelist
             return favelist.save();
@@ -146,7 +151,7 @@ function addDishToFaveList(req, res, next){
             return favelist;
         }
     })
-    .then((favelist) => success(favelist,200,res),(err) => next(err))
+    .then((favelist) => successResponse(favelist,200,res),(err) => next(err))
     .catch((err) => next(err));
 }
 module.exports = favoriteRouter;
